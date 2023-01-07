3515769bbfe0958d865b17ac6e5e7beaa8f02ca2
dnc.vladyslav.semenov@gmail.com <dnc.vladyslav.semenov@gmail.com>
2023-01-07 17:27:03 +0300

    delete token from source

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const TelegramBot = require("node-telegram-bot-api");
 const cron = require("node-cron");
 
-const token = "5924623836:AAHq7hWkwoHsZn1EzLcp034CtqVEv-1TOpo";
+const token = process.env.TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 
 const chatName = "-1001623450891";
