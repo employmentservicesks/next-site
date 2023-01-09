1d5c4f49526a573e6e3d1bd0d65dede00d3adfde
dnc.vladyslav.semenov@gmail.com <dnc.vladyslav.semenov@gmail.com>
2023-01-09 09:35:39 +0300

    edited timezone

--- bot.js
+++ bot.js
@@ -6,22 +6,28 @@ const bot = new TelegramBot(token, { polling: true });
 
 const chatName = "-1001623450891";
 
-bot.sendMessage(chatName, "MainControlBot управляет графиком работы чата");
+cron.schedule(
+  "1 18 * * *",
+  () => {
+    bot
+      .setChatPermissions(chatName, { can_send_messages: false })
+      .then(() =>
+        bot.sendMessage(chatName, "Отправка сообщений в чат запрещена до 09:00")
+      )
+      .catch((e) => console.log(e));
+  },
+  { timezone: "Europe/Moscow" }
+);
 
-cron.schedule("1 18 * * *", () => {
-  bot
-    .setChatPermissions(chatName, { can_send_messages: false })
-    .then(() =>
-      bot.sendMessage(chatName, "Отправка сообщений в чат запрещена до 09:00")
-    )
-    .catch((e) => console.log(e));
-});
-
-cron.schedule("59 8 * * *", () => {
-  bot
-    .setChatPermissions(chatName, { can_send_messages: true })
-    .then(() =>
-      bot.sendMessage(chatName, "Отправка сообщений в чат разрешена до 18:00")
-    )
-    .catch((e) => console.log(e));
-});
+cron.schedule(
+  "59 8 * * *",
+  () => {
+    bot
+      .setChatPermissions(chatName, { can_send_messages: true })
+      .then(() =>
+        bot.sendMessage(chatName, "Отправка сообщений в чат разрешена до 18:00")
+      )
+      .catch((e) => console.log(e));
+  },
+  { timezone: "Europe/Moscow" }
+);
