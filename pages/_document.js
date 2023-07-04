ca48fb01b795af023c34ad40e943ce693337d317
employmentservicesks <sluzhba_z@khogov.ru>
2023-07-04 10:22:29 +0300

    metrika

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,12 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import '../helpers/metrika'
 
 export default function Document() {
   return (
     <Html id="html" lang="en">
       <Head />
       <body>
+      <noscript><div><img src="https://mc.yandex.ru/watch/94197512" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
         <Main />
         <NextScript />
       </body>
