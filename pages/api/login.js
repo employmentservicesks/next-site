035b903e20179732ba2ff2186f1019cc57112989
employmentservicesks <sluzhba_z@khogov.ru>
2023-10-12 15:29:17 +0300

    fix

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,17 +1,17 @@
-import clientPromise from "../../lib/mongodb";
+// import clientPromise from "../../lib/mongodb";
 
-export default async (req, res) => {
-  try {
-    const client = await clientPromise;
-    const db = client.db("Employment");
+// export default async (req, res) => {
+//   try {
+//     const client = await clientPromise;
+//     const db = client.db("Employment");
 
-    const users = await db
-      .collection("Users")
-      .find({})
-      .toArray();
+//     const users = await db
+//       .collection("Users")
+//       .find({})
+//       .toArray();
 
-    res.json(users);
-  } catch (e) {
-    console.error(e);
-  }
-};
+//     res.json(users);
+//   } catch (e) {
+//     console.error(e);
+//   }
+// };
