90e6e59dec8da68e77493f95949be40045bc78af
employmentservicesks <120101836+employmentservicesks@users.noreply.github.com>
2024-05-24 13:36:32 +0300

    Update Footer.js
    Update

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ const Footer = memo(function Footer() {
   const date = new Date();
   return (
     <div className="w-full bg-gray-300 dark:bg-gray-700 text-black dark:text-white text-center align-middle py-4">
-      Служба занятости Херсонской области - {date.getFullYear()}
+      Служба занятости населения Херсонской области - {date.getFullYear()}
     </div>
   );
 });
